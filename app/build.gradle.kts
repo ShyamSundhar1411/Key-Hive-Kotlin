398f4af01224b78887a8f3b64be3555fc9b485d5
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-01-21 11:40:20 +0530

    build: version 2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.axionlabs.keyhive"
         minSdk = 29
         targetSdk = 34
-        versionCode = 4
-        versionName = "2.1"
+        versionCode = 5
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
